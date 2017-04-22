ae2c9a522d8f61ebbfffc08e3e174a721ffa5e1c
Yurii Halavai(Yuriy Galavay) <ygalavay@gmail.com>
2017-04-22 13:59:18 +0200

    Added data model for confession

--- src/app/model/commandment.ts
+++ src/app/model/commandment.ts
@@ -1,7 +1,7 @@
 import {ConcreteSin} from "./concrete_sin";
 
 export class Commandment {
-  public position: number;
+  public number: number;
   public name : String;
   public description : String;
   public sins : Array<ConcreteSin>;
---
+++ src/app/model/confession.ts
@@ -0,0 +1,7 @@
+import {Commandment} from "./commandment";
+export class Cofession {
+  public date : Date;
+  public name: string;
+  public commandments : Commandment[];
+  public finished : boolean
+}
