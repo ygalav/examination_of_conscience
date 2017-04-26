534d081771094b0245145670ba2c7a47d3c8c18b
Yurii Halavai(Yuriy Galavay) <ygalavay@gmail.com>
2017-04-26 22:50:40 +0200

    Added feature to store sins in the confession

--- src/app/views/commandment/commandment-view.component.ts
+++ src/app/views/commandment/commandment-view.component.ts
@@ -3,6 +3,7 @@ import {Commandment} from "../../model/commandment";
 import {ConcreteSin} from "../../model/concrete_sin";
 import {ConfessionService} from "../../service/confession.service";
 import {Confession} from "../../model/confession";
+import {SinViewDto} from "./sin-view.dto";
 @Component({
   templateUrl: 'commandment-view.html',
   selector: 'eoc-commandment-view'
@@ -11,22 +12,73 @@ export class CommandmentView implements OnInit {
 
   @Input() commandment : Commandment;
   @Input() confession : Confession;
+
   @Output() confessionChanged = new EventEmitter<Confession>();
+
+  sins : SinViewDto[] = [];
 
   constructor(public confessionService : ConfessionService){}
 
   ngOnInit(): void {
-    /*this.confessionService.getActiveConfession().then(
-      confession => this.confession = confession
-    )*/
+
+    this.commandment.sins.forEach(sin => {
+      let sinView = new SinViewDto;
+      sinView.sin = sin;
+
+      if (this.confession) {
+        sinView.selectedInExamination = this.isSinCheckedInConfession(sin, this.commandment, this.confession);
+      }
+      this.sins.push(sinView);
+    });
   }
 
-  toggleCommandment(commandment : Commandment, sin : ConcreteSin) {
-    let commandmentCopy = new Commandment;
-    commandmentCopy.name = commandment.name;
-    commandmentCopy.number = commandment.number;
-    commandmentCopy.sins.push(sin);
+  private isSinCheckedInConfession(sin : ConcreteSin, commandment: Commandment, confession : Confession) : boolean {
+    if (confession && confession.commandments.length > 0) {
+      let commandmentFromConfession = confession.commandments.filter(c => c.number == commandment.number)[0];
+      if (commandmentFromConfession) {
+        return this.isCommandmentHasASin(commandmentFromConfession, sin)
+      }
+    }
+    return false;
+  }
 
-    this.confession.commandments.push(commandmentCopy);
+  private isCommandmentHasASin(commandment : Commandment, sin : ConcreteSin) : boolean {
+    let result = false;
+    if (sin && commandment && commandment.sins) {
+      return commandment.sins.filter(aSin => aSin.name === sin.name).length > 0;
+    }
+    return result;
+  }
+
+  toggleCommandment(commandmentFromUI : Commandment, sin : ConcreteSin) {
+    let commandments = this.confession.commandments.filter(c => commandmentFromUI.number === c.number);
+    let hasCommandmentWithNumber = commandments.length > 0;
+
+    let commandmentFromConfession : Commandment = null;
+    if (hasCommandmentWithNumber) {
+      commandmentFromConfession = commandments[0];
+    } else {
+      //create new commandment and copy info to the copy
+      commandmentFromConfession = new Commandment;
+      commandmentFromConfession.name = commandmentFromUI.name;
+      commandmentFromConfession.number = commandmentFromUI.number;
+    }
+
+    //If sin is in the confession - then remove
+    if (this.isCommandmentHasASin(commandmentFromConfession, sin)) {
+      //Remove sin from commandment
+      commandmentFromConfession.sins = commandmentFromConfession.sins.filter(aSin => aSin.name !== sin.name);
+    }
+    else {
+      commandmentFromConfession.sins.push(sin);
+    }
+
+    //Remove current
+    this.confession.commandments =
+      this.confession.commandments.filter(c => c.number !== commandmentFromConfession.number);
+    //Add new Current
+    this.confession.commandments.push(commandmentFromConfession);
+    //Save
+    this.confessionService.saveConfession(this.confession).then(()=>{})
   }
 }
---
+++ src/app/views/commandment/sin-view.dto.ts
@@ -0,0 +1,6 @@
+import {ConcreteSin} from "../../model/concrete_sin";
+export class SinViewDto
+{
+  public sin : ConcreteSin;
+  public selectedInExamination : boolean;
+}
